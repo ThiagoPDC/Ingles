46f2e2eccb8abe2741bd27ef3d80bde9ce7578aa
ThiagoPDC <thiagopavandallacosta@gmail.com>
2023-06-21 17:12:13 -0300

    translating the page

--- index.php
+++ index.php
@@ -15,51 +15,47 @@
                 <p class="title">USP - Engenharia Da Computação</p>
             </div>
             <div class="card body bg-dark ">
-                <p class="header1 m-2">Sobre o curso</p>
+                <p class="header1 m-2">About the specialization</p>
                 <p class="normal-text m-2">
-                    <img src="/indexImage/usp_campus_sp.jpg" class="rounded corner col-sm-4 imgR m-2 img-fluid">
-                    O objetivo do curso de Engenharia de Computação é a formação de profissionais plenamente 
-                    capacitados para exercer atividades relacionadas com áreas de interface entre engenharia 
-                    elétrica e ciências da computação. Acompanhando as rápidas mudanças observadas em diversos 
-                    setores da atividade econômica, são oferecidas aos alunos, por intermédio de disciplinas 
-                    complementares, ênfases para o aperfeiçoamento do perfil profissional em sistemas embarcados, 
-                    sistemas de comunicações e computação móvel, sistemas computacionais avançados e robótica. 
-                    A proposta contempla um perfil diferenciado em relação a diversos cursos de engenharia de
-                    computação já implantados no país e faz uso da ampla experiência pedagógica, científica e 
-                    tecnológica da EESC e do ICMC.  
+                    <img src="/indexImage/usp_campus_sp.jpg" class="rounded corner col-sm-4 imgR m-2 img-fluid">                 
+                    The objective of the Computer Engineering course is to train professionals who are fully qualified to carry out activities 
+                    related to interface areas between electrical engineering and computer science. Accompanying the rapid changes observed in
+                    various sectors of economic activity, students are offered, through complementary disciplines, emphasis on improving their 
+                    professional profile in embedded systems, communications systems and mobile computing, advanced computing systems and robotics. 
+                    The proposal contemplates a differentiated profile in relation to several computer engineering courses already implemented in the 
+                    country and makes use of the wide pedagogical, scientific and technological experience of EESC and ICMC.  
                 </p>
-                <p class="header1 m-2">O que você vai estudar ? </p>
+                <p class="header1 m-2">What you're going to study ?</p>
                 <p class="normal-text m-2">
-                    Placas e circuitos serão seus companheiros nessa jornada. Disciplinas como elétrica e eletrônica, 
-                    química e física também marcarão presença. É possível, ainda, especializar-se em ênfases como robótica 
-                    e sistemas embarcados. Por exemplo, dentro de um carro há um sistema que o controla, um sistema embarcado, 
-                    que está presente em quase todas as máquinas que usamos em nosso dia a dia, como os eletrodomésticos.
+                    Boards and circuits will be your companions on this journey. Disciplines such as electrical and electronics, chemistry and 
+                    physics will also be present. It is also possible to specialize in emphases such as robotics and embedded systems. For 
+                    example, inside a car there is a system that controls it, an embedded system, which is present in almost all the machines that 
+                    we use in our daily lives, such as home appliances.
                     <br><br>
-                    A matriz curricular tem um núcleo básico constituído por um grupo de disciplinas que reúne física, matemática, 
-                    química, humanidades, administração e economia. O núcleo profissionalizante proporciona um perfil fortemente baseado 
-                    em engenharia eletrônica, computação e áreas de interface, e engloba o conteúdo das áreas de: processamento digital 
-                    de sinais, microeletrônica (projetos de circuitos integrados analógicos e digitais), circuitos digitais, robótica e 
-                    automação, telecomunicações, engenharia de software, sistemas computacionais distribuídos, computação gráfica, banco de dados, 
-                    hipermídia e multimídia, programação matemática. O núcleo de especialização é composto por conjunto de disciplinas de livre escolha, 
-                    compreendendo as seguintes ênfases: sistemas embarcados, sistemas de comunicações e computação móvel, sistemas computacionais 
-                    avançados e robótica.
+                    The curricular matrix has a basic core made up of a group of disciplines that bring together physics, mathematics, chemistry, 
+                    humanities, administration and economics. The professional core provides a profile strongly based on electronic engineering, 
+                    computing and interface areas, and encompasses content from the areas of: digital signal processing, microelectronics (analog 
+                    and digital integrated circuit design), digital circuits, robotics and automation, telecommunications , software engineering, distributed 
+                    computing systems, computer graphics, database, hypermedia and multimedia, mathematical programming. The core of specialization is made up 
+                    of a set of subjects of free choice, comprising the following emphases: embedded systems, communications systems and mobile computing, 
+                    advanced computing systems and robotics.
                 </p>
-                <p class="header1 m-2">O que você vai fazer ?</p>
+                <p class="header1 m-2">What you're goint to do ?</p>
                 <p class="normal-text m-2">
-                    Formando-se em Engenharia de Computação, você poderá projetar sistemas digitais, computadorizados e até robôs. Outra possibilidade 
-                    é gerenciar redes de computadores em grandes empresas e dar assistência a elas, ou então, desenvolver aplicativos e também atuar 
-                    no ramo de automação industrial. As áreas de atuação estão situadas na interface científica e tecnológica entre a engenharia elétrica/eletrônica 
-                    e a ciência da computação. Você poderá atuar em áreas da engenharia eletrônica, computação, telecomunicações, microeletrônica, automação, 
-                    tecnologia da informação, processamento de voz, som e imagem, inteligência artificial, robótica, engenharia de software, redes de computadores, 
-                    sistemas computacionais distribuídos, computação paralela e de alto desempenho, banco de dados e ferramentas auxiliadas por computador.
-                    <br><br>
-                    O engenheiro de computação se destaca por sua versatilidade aliada ao seu conhecimento técnico-científico: não basta conhecer bem um 
-                    determinado assunto, é necessário saber transmiti-lo e saber aplicá-lo na resolução de problemas. Ele tem condições de atuar em 
-                    diversos segmentos e isso força uma constante busca por atualização técnico-científica. A versatilidade da formação permite ao engenheiro 
-                    de computação atuar tanto em hardware como em software, o que o torna um profissional bastante solicitado pelo mercado. O mercado, de modo 
-                    geral, reconhece o valor do engenheiro de computação, o que é refletido em uma ascensão natural na carreira.
+                    By graduating in Computer Engineering, you will be able to design digital, computerized systems and even robots. Another 
+                    possibility is to manage computer networks in large companies and provide assistance to them, or develop applications and 
+                    also work in the field of industrial automation. The areas of expertise are situated at the scientific and technological 
+                    interface between electrical/electronic engineering and computer science. You will be able to work in areas of electronic 
+                    engineering, computing, telecommunications, microelectronics, automation, information technology, voice, sound and image 
+                    processing, artificial intelligence, robotics, software engineering, computer networks, distributed computing systems, 
+                    parallel computing and high performance, database and computer-aided tools. The computer engineer stands out for his 
+                    versatility combined with his technical-scientific knowledge: it is not enough to know a certain subject well, it is 
+                    necessary to know how to transmit it and how to apply it in solving problems. He is able to operate in several segments and 
+                    this forces a constant search for technical-scientific updating. The versatility of the training allows the computer engineer 
+                    to work both in hardware and software, which makes him a highly sought after professional by the market. The market, in general, 
+                    recognizes the value of the computer engineer, which is reflected in a natural rise in the career.
                     <br> 
-                    <a href="https://uspdigital.usp.br/jupiterweb/listarGradeCurricular?codcg=97&codcur=97001&codhab=0&tipo=N">Confira a grade curricular aqui!</a>
+                    <a href="https://uspdigital.usp.br/jupiterweb/listarGradeCurricular?codcg=97&codcur=97001&codhab=0&tipo=N">Check out the cuniversity curriculum!</a>
                 </p>
                 
                 <hr>
