fa8190fe0d1c9603576e5b09732c762ba84f57ca
ThiagoPDC <thiagopavandallacosta@gmail.com>
2023-06-21 15:47:44 -0300

    site responsivo curso da unicamp

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>ThiagoPDC</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link href="/style.css" rel="stylesheet">
+</head>
+<body>
+     <div class="container mt-5"> 
+        <div class="row">
+            <div class="card header bg-dark">
+                <p class="title">USP - Engenharia Da Computação</p>
+            </div>
+            <div class="card body bg-dark ">
+                <p class="header1 m-2">Sobre o curso</p>
+                <p class="normal-text m-2">
+                    <img src="/indexImage/usp_campus_sp.jpg" class="rounded corner col-sm-4 imgR m-2 img-fluid">
+                    O objetivo do curso de Engenharia de Computação é a formação de profissionais plenamente 
+                    capacitados para exercer atividades relacionadas com áreas de interface entre engenharia 
+                    elétrica e ciências da computação. Acompanhando as rápidas mudanças observadas em diversos 
+                    setores da atividade econômica, são oferecidas aos alunos, por intermédio de disciplinas 
+                    complementares, ênfases para o aperfeiçoamento do perfil profissional em sistemas embarcados, 
+                    sistemas de comunicações e computação móvel, sistemas computacionais avançados e robótica. 
+                    A proposta contempla um perfil diferenciado em relação a diversos cursos de engenharia de
+                    computação já implantados no país e faz uso da ampla experiência pedagógica, científica e 
+                    tecnológica da EESC e do ICMC.  
+                </p>
+                <p class="header1 m-2">O que você vai estudar ? </p>
+                <p class="normal-text m-2">
+                    Placas e circuitos serão seus companheiros nessa jornada. Disciplinas como elétrica e eletrônica, 
+                    química e física também marcarão presença. É possível, ainda, especializar-se em ênfases como robótica 
+                    e sistemas embarcados. Por exemplo, dentro de um carro há um sistema que o controla, um sistema embarcado, 
+                    que está presente em quase todas as máquinas que usamos em nosso dia a dia, como os eletrodomésticos.
+                    <br><br>
+                    A matriz curricular tem um núcleo básico constituído por um grupo de disciplinas que reúne física, matemática, 
+                    química, humanidades, administração e economia. O núcleo profissionalizante proporciona um perfil fortemente baseado 
+                    em engenharia eletrônica, computação e áreas de interface, e engloba o conteúdo das áreas de: processamento digital 
+                    de sinais, microeletrônica (projetos de circuitos integrados analógicos e digitais), circuitos digitais, robótica e 
+                    automação, telecomunicações, engenharia de software, sistemas computacionais distribuídos, computação gráfica, banco de dados, 
+                    hipermídia e multimídia, programação matemática. O núcleo de especialização é composto por conjunto de disciplinas de livre escolha, 
+                    compreendendo as seguintes ênfases: sistemas embarcados, sistemas de comunicações e computação móvel, sistemas computacionais 
+                    avançados e robótica.
+                </p>
+                <p class="header1 m-2">O que você vai fazer ?</p>
+                <p class="normal-text m-2">
+                    Formando-se em Engenharia de Computação, você poderá projetar sistemas digitais, computadorizados e até robôs. Outra possibilidade 
+                    é gerenciar redes de computadores em grandes empresas e dar assistência a elas, ou então, desenvolver aplicativos e também atuar 
+                    no ramo de automação industrial. As áreas de atuação estão situadas na interface científica e tecnológica entre a engenharia elétrica/eletrônica 
+                    e a ciência da computação. Você poderá atuar em áreas da engenharia eletrônica, computação, telecomunicações, microeletrônica, automação, 
+                    tecnologia da informação, processamento de voz, som e imagem, inteligência artificial, robótica, engenharia de software, redes de computadores, 
+                    sistemas computacionais distribuídos, computação paralela e de alto desempenho, banco de dados e ferramentas auxiliadas por computador.
+                    <br><br>
+                    O engenheiro de computação se destaca por sua versatilidade aliada ao seu conhecimento técnico-científico: não basta conhecer bem um 
+                    determinado assunto, é necessário saber transmiti-lo e saber aplicá-lo na resolução de problemas. Ele tem condições de atuar em 
+                    diversos segmentos e isso força uma constante busca por atualização técnico-científica. A versatilidade da formação permite ao engenheiro 
+                    de computação atuar tanto em hardware como em software, o que o torna um profissional bastante solicitado pelo mercado. O mercado, de modo 
+                    geral, reconhece o valor do engenheiro de computação, o que é refletido em uma ascensão natural na carreira.
+                    <br> 
+                    <a href="https://uspdigital.usp.br/jupiterweb/listarGradeCurricular?codcg=97&codcur=97001&codhab=0&tipo=N">Confira a grade curricular aqui!</a>
+                </p>
+                
+                <hr>
+                
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
